fc288a9aeda678cb938dac8c7520ad646fed70c7
fcourt <108086687+fcourt@users.noreply.github.com>
2026-04-24 16:31:46 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { WalletProvider } from './context/WalletContext'
 import App from './App'
+import './i18n'
 import './styles/global.css'
 
 createRoot(document.getElementById('root')).render(
