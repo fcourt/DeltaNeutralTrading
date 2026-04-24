9194650249cadbb2f973297dcbca1d585030ca06
fcourt <108086687+fcourt@users.noreply.github.com>
2026-04-24 16:18:56 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './i18n.js'
+import { WalletProvider } from './context/WalletContext'
+import App from './App'
 import './styles/global.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
-      <App />
+      <WalletProvider>
+        <App />
+      </WalletProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 )
