dfedb9aaadac2985aeacca764b1ccd709b60c771
fcourt <108086687+fcourt@users.noreply.github.com>
2026-04-25 20:39:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// ✅ CORRECTION
-export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { '@': '/src' } },
-  server: {
-    proxy: {
-      '/api/extended': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/api/nado': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
-})
+ 
+ export default defineConfig({
+   plugins: [react()],
+   resolve: {
+     alias: {
+       '@': '/src',
+     },
+   },
+ })
