d1e293db2718a1db7d9d8640e081388c0ee70990
fcourt <108086687+fcourt@users.noreply.github.com>
2026-05-05 21:09:07 +0200

    Update SettingKeys.jsx

--- src/pages/SettingKeys.jsx
+++ src/pages/SettingKeys.jsx
@@ -1,5 +1,5 @@
 // src/pages/SettingKeys.jsx
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useWallet } from '../context/WalletContext'
 
